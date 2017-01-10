af228ebd6661421682eedbe1e4199de789f16460
deNiroMe <dwainir@gmail.com>
2017-01-10 22:39:59 +0000

    add a comment for test

--- src/main/java/edu/school/mgmt/config/SecurityWebApplicationInitializer.java
+++ src/main/java/edu/school/mgmt/config/SecurityWebApplicationInitializer.java
@@ -3,5 +3,5 @@ package edu.school.mgmt.config;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
- 
+ // a comment 
 }
